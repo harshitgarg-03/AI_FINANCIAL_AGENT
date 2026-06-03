bdf82bea2296453c5b99ca24ddb2c1764dae77cd
Harshit Garg~ <gargh2120@gmail.com>
2026-06-03 16:22:57 +0530

    DB queries and connections

--- app/db/queries.py
+++ app/db/queries.py
@@ -3,19 +3,20 @@ from app.db.postgres import engine
 
 
 def fetch_user_transactions(user_id: str):
-    print("user id  in queries " , user_id)
+    # print("user id  in queries " , user_id)
     query = text("""
         SELECT *
-        FROM transactions
-        WHERE user_id = :user_id
+        FROM "Transaction"
+        WHERE userId = :user_id
         ORDER BY date DESC
-    """)
+    """),
+    {"userId": user_id}
 
     with engine.connect() as conn:
 
         result = conn.execute(
             query,
-            {"user_id": user_id}
+            {"userId": user_id}
         )
 
         return [dict(row._mapping) for row in result]
