be9b808300d2d8b4a4732a3aa9101cbe1ec86de5
Harshit Garg~ <gargh2120@gmail.com>
2026-08-20 22:08:19 +0530

    fix error

--- app/db/postgres.py
+++ app/db/postgres.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
-# from langgraph.checkpoint.postgres import PostgresSaver
 
 import os;
 
@@ -9,16 +8,3 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL");
 
 engine = create_engine(DATABASE_URL);
-
-from sqlalchemy import text
-
-# with engine.connect() as conn:
-#     result = conn.execute(text("SELECT 1"))
-#     print(result.fetchone())
-
-
-# def Checkpointer() :
-#     with PostgresSaver.from_conn_string(DATABASE_URL) as checkpointer:
-#         checkpointer.__exit__(None, None, None)
-#         checkpointer.setup()
-#         return checkpointer  
