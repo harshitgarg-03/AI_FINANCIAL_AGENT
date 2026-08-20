107c47a8d728c6efcf83d86a1c6eded25a6c89c6
Harshit Garg~ <gargh2120@gmail.com>
2026-08-20 21:56:15 +0530

    fix error

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-import uvicorn
-from app.agent.financial_agent import FinancialAgent
 from contextlib import asynccontextmanager
+import uvicorn
+import traceback
 
+from app.agent.financial_agent import FinancialAgent
 
-app = FastAPI(title="AI Financial Agent API")
-
-# Add CORS middleware to allow requests from frontend
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 agent = FinancialAgent()
+
 
 class ChatRequest(BaseModel):
     user_id: str
@@ -28,27 +20,44 @@ class ChatRequest(BaseModel):
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     global agent
-    agent = FinancialAgent()
-    yield
-    agent.cleanup()  # Add a cleanup() method to FinancialAgent
 
-app = FastAPI(title="AI Financial Agent API", lifespan=lifespan)
+    agent = FinancialAgent()
+
+    yield
+
+    agent.cleanup()
+
+
+app = FastAPI(
+    title="AI Financial Agent API",
+    lifespan=lifespan
+)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.post("/api/chat")
 def chat_endpoint(request: ChatRequest):
 
-    # print("hello inm main .py ", request.user_id, request.question, request, request.thread_id)
     try:
         response = agent.chat(
             user_id=request.user_id,
             question=request.question,
             thread_id=request.thread_id
         )
-        print("response is ::", response)
-        return {"response":   response}
+
+        return {
+            "response": response
+        }
+
     except Exception as e:
-        import traceback
 
         print("ERROR OCCURRED:")
         traceback.print_exc()
@@ -58,9 +67,20 @@ def chat_endpoint(request: ChatRequest):
             detail=str(e)
         )
 
+
 @app.get("/")
 def home():
-    return {"status": "ok", "message": "AI Financial Agent is running"}
+
+    return {
+        "status": "ok",
+        "message": "AI Financial Agent is running"
+    }
+
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run(
+        "main:app",
+        host="127.0.0.1",
+        port=8000,
+        reload=True
+    )
